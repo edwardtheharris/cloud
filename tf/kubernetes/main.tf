e2b36fad4f0939d00a0aaa47e9131968f6309339
Xander Harris <plebis@hotmail.com>
2018-10-30 23:54:21 +0000

    Add travis. #1

--- tf/kubernetes/main.tf
+++ tf/kubernetes/main.tf
@@ -15,16 +15,19 @@ resource "linode_instance" "kubernetes-actual" {
   root_pass       = "${var.linode_token}"
 }
 
-#resource "linode_instance" "kubernetes-alpha" {
-#  image = "linode/arch"
-#  label = "kubernetes-alpha"
-#  group = "kubernetes"
-#  region = "us-west"
-#  type = "g6-nanode-1"
-#  authorized_keys = [ "${var.pubkey}" ]
-#  root_pass = "${var.linode_token}"
-#}
+resource "linode_instance" "kubernetes-alpha" {
+  image = "linode/arch"
+  label = "kubernetes-alpha"
+  group = "kubernetes"
+  region = "us-west"
+  type = "g6-nanode-1"
+  authorized_keys = [ "${var.pubkey}" ]
+  root_pass = "${var.linode_token}"
+}
 
 output "inventory_hostname" {
-  value = "${linode_instance.kubernetes-actual.ip_address}"
+  value = [
+    "${linode_instance.kubernetes-actual.ip_address}",
+    "${linode_instance.kubernetes-alpha.ip_address}"
+  ]
 }
