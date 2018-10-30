66d29b80dda89311974ccd8ccf87ce8160e3560a
Xander Harris <plebis@hotmail.com>
2018-10-30 14:49:51 -0700

    Adjust directory, add playbook. #2

--- tf/kubernetes/kubernetes.tf
+++ tf/kubernetes/kubernetes.tf
@@ -6,38 +6,13 @@ provider "linode" {
 }
 
 resource "linode_instance" "kubernetes-actual" {
-  image = "linode/arch"
-  label = "kuberenetes-actual"
-  group = "kubernetes"
-  region = "us-west"
-  type = "g6-nanode-1"
-  authorized_keys = [ "${var.pubkey}" ]
-  root_pass = "${var.linode_token}"
-
-  provisioner "local-exec" {
-    command = "ansible-playbook wait.yml"
-  }
-
-  provisioner "local-exec" {
-    command = "cd ../; ansible-playbook archlinux.yml"
-  }
-}
-
-resource "ansible_host" "kubernetes-actual" {
-    inventory_hostname = "${kubernetes-actual.ip_address}"
-    groups = ["kubernetes"]
-    vars {
-        ansible_user = "root"
-    }
-}
-
-resource "ansible_group" "kubernetes" {
-  inventory_group_name = "kubernetes"
-  # children = ["foo", "bar", "baz"]
-  # vars {
-  #   foo = "bar"
-  #   bar = 2
-  # }
+  image           = "linode/arch"
+  label           = "kuberenetes-actual"
+  group           = "kubernetes"
+  region          = "us-west"
+  type            = "g6-nanode-1"
+  authorized_keys = ["${var.pubkey}"]
+  root_pass       = "${var.linode_token}"
 }
 
 #resource "linode_instance" "kubernetes-alpha" {
@@ -49,3 +24,7 @@ resource "ansible_group" "kubernetes" {
 #  authorized_keys = [ "${var.pubkey}" ]
 #  root_pass = "${var.linode_token}"
 #}
+
+output "inventory_hostname" {
+  value = "${linode_instance.kubernetes-actual.ip_address}"
+}
